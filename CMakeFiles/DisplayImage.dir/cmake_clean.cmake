b3efe00842a9ff417446151cc19c489a07aa5c87
olavmulder02@gmail.com <olavmulder02@gmail.com>
2021-06-03 11:58:57 +0100

    ain

---
+++ CMakeFiles/DisplayImage.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DisplayImage.dir/main.cpp.o"
+  "CMakeFiles/DisplayImage.dir/src/DataCamera.cpp.o"
+  "CMakeFiles/DisplayImage.dir/src/DistanceSensor.cpp.o"
+  "CMakeFiles/DisplayImage.dir/src/I2C.cpp.o"
+  "CMakeFiles/DisplayImage.dir/src/Location.cpp.o"
+  "CMakeFiles/DisplayImage.dir/src/Map.cpp.o"
+  "CMakeFiles/DisplayImage.dir/src/Motor.cpp.o"
+  "CMakeFiles/DisplayImage.dir/src/Route.cpp.o"
+  "CMakeFiles/DisplayImage.dir/src/Target.cpp.o"
+  "DisplayImage"
+  "DisplayImage.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DisplayImage.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
